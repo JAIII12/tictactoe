ae90a59a5458bae980295b2d1e781c40ef1d4812
JAI <sy9534411@gmail.com>
2024-09-29 00:23:18 +0530

    tt

--- script.js
+++ script.js
@@ -1,51 +1,71 @@
-console.log("Welcome to tic tac toe")
-let music=new Audio("music.mp3")
-let audioTurn =new Audio("ting.mp3")
-let gameover=new Audio("gameover.mp3")
-let turn = "X"
-let isgameover= false;
+console.log("Welcome to tic tac toe");
+let music = new Audio("music.mp3");
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
+let isgameover = false;
 
+// Play background music
+// music.play();
 
-const changeTurn= ()=>{
-      return turn=="X"?"0":"X"
+const changeTurn = () => {
+    return turn === "X" ? "O" : "X";
+};
 
-}
-
-const checkWin = ()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
-    let wins= [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
-    ]
-    wins.forEach(e=>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && boxtext[e[0]].innerText !== ""){
-            document.querySelector('.info').innnerText = boxtext[e[0]].innerText + " Won "
-            isgameover =  true
-       }
- })
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ];
+    
+    wins.forEach(e => {
+        if (boxtext[e[0]].innerText === boxtext[e[1]].innerText && 
+            boxtext[e[2]].innerText === boxtext[e[1]].innerText && 
+            boxtext[e[0]].innerText !== "") {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won!";
+            isgameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            gameover.play();
+            document.querySelector(".line").style.width = '20px';
+            // Corrected template literals usage
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+        }
+    });
+};
 
-    }
-
-
-
-let boxes =document.getElementsByClassName("box");
-Array.from(boxes).forEach(element=>{
-    let boxtext= element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText ===''){
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '' && !isgameover) {
             boxtext.innerText = turn;
-            turn=changeTurn();
             audioTurn.play();
             checkWin();
-            if(!isgameover){
-            document.getElementsByClassName("info")[0].innerText = "Turn for" + turn; 
+            if (!isgameover) {
+                turn = changeTurn();
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
             }
         }
-    })
-})
+    });
+});
+
+// Select the reset button
+let reset = document.getElementById('reset');
+reset.addEventListener('click', () => {
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = "";
+    });
+    turn = "X"; // Ensure consistency in case-sensitive
+    isgameover = false;
+    document.querySelector(".line").style.width = '0px';
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+});
